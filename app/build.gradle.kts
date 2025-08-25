ef04c28d88b902567e67827eff06ef17e0b63a77
BasireddyKarthik <karthikdeveloper45@gmail.com>
2025-08-25 11:52:23 +0530

    fdf

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.kfinone.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
+        versionCode = 16
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
